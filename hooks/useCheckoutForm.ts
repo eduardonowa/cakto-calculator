355b6a2853ba59a58de7ae3c410352d39ea9010f
Eduardo Nowakoski <eduardo_nowa@hotmail.com>
2026-01-31 18:25:22 -0300

    fix: removing logs used for debug

--- hooks/useCheckoutForm.ts
+++ hooks/useCheckoutForm.ts
@@ -63,8 +63,6 @@ export function useCheckoutForm(product: IProduct) {
           await submitFn();
         } else {
           await new Promise((r) => setTimeout(r, 1500));
-          console.log('email', email);
-          console.log('MOCK_ERROR_EMAIL', MOCK_ERROR_EMAIL);
           if (email === MOCK_ERROR_EMAIL) {
             throw new Error('Erro ao processar. Tente novamente.');
           }
